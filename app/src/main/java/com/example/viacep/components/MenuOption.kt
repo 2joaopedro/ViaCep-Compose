e705d283c064cedc70b77c275bdc563a45df090a
2joaopedro <joao.pedro.franchini.4@gmail.com>
2024-03-23 17:30:21 -0300

    Fiz o componente SearchBar

--- app/src/main/java/com/example/viacep/components/MenuOption.kt
+++ app/src/main/java/com/example/viacep/components/MenuOption.kt
@@ -6,10 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -30,24 +28,15 @@ fun MenuOption(modifier: Modifier = Modifier) {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(5.dp)
     ) {
-        Spacer(
-            modifier
-                .size(5.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFC595FF))
-        )
-        Spacer(
-            modifier
-                .size(5.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFC595FF))
-        )
-        Spacer(
-            modifier
-                .size(5.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFC595FF))
-        )
+
+        repeat(3) {
+            Spacer(
+                modifier
+                    .size(5.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFC595FF))
+            )
+        }
     }
 }
 
